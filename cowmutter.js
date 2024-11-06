e7b7d985c7f07948d229093e7b8b1fdb37837fc5
☆ stargen ☆ <appakling@mail.com>
2024-11-06 14:36:18 -0600

    Update and rename cowsay.js to cowmutter.js

--- cowmutter.js
+++ cowmutter.js
@@ -1,6 +1,12 @@
 /** @param {NS} ns */
-// COWSAY FOR BITBURNER
-// BY HENRY BERMAN
+/** COWMUTTER: COWSAY DEMAKE FOR BITBURNER
+ * custom script by starg3n/appak
+ * 
+ * DOCS:
+ * read setup.txt
+ * > cowsay [message]
+ * 
+ */
 export async function main(ns) {
 
   let speech = ns.args[0]; // speech added onto cow
