e3f1496f0b3fe1b86681bae4615047b868938a60
☆ stargen ☆ <appakling@mail.com>
2024-11-07 13:37:06 -0600

    cowmutter overhaul 2.0

--- cowmutter.js
+++ cowmutter.js
@@ -1,22 +1,23 @@
 /** @param {NS} ns */
 /** COWMUTTER: COWSAY DEMAKE FOR BITBURNER
+ * VERSION 2.0
  * custom script by starg3n/appak
  * 
  * DOCS:
  * read setup.txt
- * > cowsay [message]
+ * > cowmutter [message]
  * 
  */
 export async function main(ns) {
 
   let speech = ns.args[0]; // speech added onto cow
-  
-  let cow = `
- ^__^
- (oo)\_______
- (__)       )--
-     ||----w |
-     ||     ||` // cow ascii art modified for bitburner
-  
-  ns.tprint(cow + speech) // I'm too stupid to make this look nice
+   
+  ns.tprint("<", speech, ">") // makeshift speech bubble
+  ns.tprint("\/")
+  ns.tprint("^__^")
+  ns.tprint("(oo)\_______")
+  ns.tprint("(__)       )--")
+  ns.tprint("    ||----w |")
+  ns.tprint("    ||     ||")
+
 }
